20c516c4a5496131a4e554ae1f974f804d9280ad
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-03-27 18:50:26 +0530

    conversion operations

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,24 +1,44 @@
-let score = "33"
+// let score = "33"
 
-// const{score}=requestAnimationFrame.body
+// // const{score}=requestAnimationFrame.body
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
  
-// "33"=> 33
-// "33abc"=> NaN
+// // "33"=> 33
+// // "33abc"=> NaN
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-let someNumber = 44
+// let someNumber = 44
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// ******************************** Operations **************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+
+let str1 = "swapnil"
+let str2 = " verma"
+console.log(str1+str2)
+
+console.log("1"+2)
+console.log(1 +"2")
+console.log("1"+2 +2)
+
+console.log(1 + 3 +"5")
