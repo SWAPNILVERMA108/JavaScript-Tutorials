9769e1f581154daa02008dbffb840bb710e78b66
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-03-27 17:40:46 +0530

    turorial

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "swapnilverma@gmail.com";
+var accountPassword = "12345";
+accountCity = "Lucknow"
+// accountId = 5
+
+console.log(accountId)
+
+/*
+prefer to not use var
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId , accountEmail , accountPassword,accountCity])
---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version     
+
+// alert(3+3)    // we are using nodejs, not browser
+
+
+console.log("swapnil verma")
+
+let name = "swapnil verma"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// bigint
+
+// string => ""
+// null => standalone value
+// undefined => 
+
+console.log(typeof "swapnil")
+
+console.log(typeof null)
+
+console.log(typeof undefined)
