42d4016c6e4ff78ec2659142b910f4af62a34d99
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-03-27 18:04:18 +0530

    conversion of datatype

---
+++ 03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33"
+
+// const{score}=requestAnimationFrame.body
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+ 
+// "33"=> 33
+// "33abc"=> NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
