401b7979694e1d67756b6e0675455593c0ad8bf3
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-03-28 19:01:04 +0530

    stack and heap memory allocation

---
+++ 01_basic/04_DataType_summary.js
@@ -0,0 +1,22 @@
+// Stack and heap memory 
+
+// Stack (primitive )  Heap (Non primptive)
+
+let nickName = "Aman"
+
+let secondName = nickName
+
+console.log(secondName)
+
+let user1 = {
+    email: "google.com",
+    upi : "user@ybl"
+
+}
+
+let user2 = user1
+
+user2.email = "swapnil@google.com"
+
+console.log(user1)
+console.log(user2)
