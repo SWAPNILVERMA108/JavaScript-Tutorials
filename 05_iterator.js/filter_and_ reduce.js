274a50d2b19a9ded1ef5c4f2b6aa8b3c0c64d39c
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-06-14 20:32:10 +0530

    basic complete

--- 05_iterator.js/filter_and_ reduce.js
+++ 05_iterator.js/filter_and_ reduce.js
@@ -1,23 +1,34 @@
-const coding =["java","js","cpp","python"]
+// const coding =["java","js","cpp","python"]
 
-const value = coding.forEach((item)=>{
-    console.log(item);
-    return item
-}) 
+// const value = coding.forEach((item)=>{
+//     console.log(item);
+//     return item
+// }) 
 
-console.log(value)
+// console.log(value)
 
 
-const num = [1,2,4,3,5,6,7,8,9]
+// const num = [1,2,4,3,5,6,7,8,9]
 
-const newNum =num.filter((nu)=>
-    nu>4
-)
+// const newNum =num.filter((nu)=>
+//     nu>4
+// )
 
-console.log(newNum)
+// console.log(newNum)
 
 // Note : if you use {} in filter function then you need to use return keyword
 
 
 // reduce fuction 
 
+
+const mynumber = [1,2,3]
+// const myTotal = mynumber.reduce(function(acc,currVal){
+//     console.log(`acc:${acc} and current value : ${currVal}`)
+//     return acc + currVal
+// },0)
+ 
+// console.log(myTotal)
+
+const myTotal = mynumber.reduce((acc,currVal)=> acc+currVal,0)
+console.log(myTotal)
